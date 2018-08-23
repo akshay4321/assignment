eba7f8bdce540961e182e97ab1bd6b2e410b8990
akshay4321 <37257885+akshay4321@users.noreply.github.com>
2018-08-23 15:14:50 +0530

    testing

---
+++ lmage/Default.aspx.vb
@@ -0,0 +1,16 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+   
+
+    Protected Sub DropDownList1_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles DropDownList1.SelectedIndexChanged
+        If DropDownList1.Text = "0" Then
+            Image1.ImageUrl = "~//8.jpg"
+        ElseIf DropDownList1.Text = "1" Then
+            Image1.ImageUrl = "~//CBR.JPG"
+        Else
+            Image1.ImageUrl = "~//2.jpg"
+        End If
+    End Sub
+End Class
