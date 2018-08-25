da3a282c1e0489d6feda9ba55be8e30da1903dc5
akshay4321 <37257885+akshay4321@users.noreply.github.com>
2018-08-25 20:59:17 +0530

    updated searching problem

--- TwitterChallenge/js/script.js
+++ TwitterChallenge/js/script.js
@@ -98,19 +98,4 @@ $(document).ready(function() {
     });
     fetchUserInfo();
 });
-$(function () {
-    $('.search-box').autocomplete({
-        search: function (event, ui) {
-            $('.searchMessage').show();
-        },
-        open: function (event, ui) {
-            $('.searchMessage').hide();
-        },
-        source: 'controller.php?autosearch=true',
-        minLength: 1
-    }).off('blur').on('blur', function () {
-        if (document.hasFocus()) {
-            $('ul.ui-autocomplete').hide();
-        }
-    });
-});
+
